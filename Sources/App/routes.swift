99107d8ed9ef264ecd08ce07f8092e42c029132c
kazki <ili.o0o@me.com>
2018-10-24 00:34:22 +0900

    Add routes to support CRUD

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -12,10 +12,37 @@ public func routes(_ router: Router) throws {
 		return "Hello, world!"
 	}
 	
+	/// Create (Post) a new acronym
 	router.post("api", "acronyms") { req -> Future<Acronym> in
 		return try req.content.decode(Acronym.self)
-		.flatMap(to: Acronym.self) { acronym in
+			.flatMap(to: Acronym.self) { acronym in
+				return acronym.save(on: req)
+			}
+	}
+	
+	/// Get all acronyms
+	router.get("api", "acronyms") { req -> Future<[Acronym]> in
+		return Acronym.query(on: req).all()
+	}
+	
+	/// Get an acronym specified by id
+	router.get("api", "acronyms", Acronym.parameter) { req -> Future<Acronym> in
+		return try req.parameters.next(Acronym.self)
+	}
+	
+	/// Update (Put) an acronym specified by id
+	router.put("api", "acronyms", Acronym.parameter) { req -> Future<Acronym> in
+		return try flatMap(to: Acronym.self, req.parameters.next(Acronym.self), req.content.decode(Acronym.self)) { acronym, updatedAcronym in
+			acronym.short = updatedAcronym.short
+			acronym.long = updatedAcronym.long
 			return acronym.save(on: req)
 		}
+	}
+	
+	/// Delete an acronym specified by id
+	router.delete("api", "acronyms", Acronym.parameter) { req -> Future<HTTPStatus> in
+		return try req.parameters.next(Acronym.self)
+			.delete(on: req) // “Fluent allows you to call delete(on:) directly on that Future. This helps tidy up code and reduce nesting. Fluent provides convenience functions for delete, update, create and save.”
+			.transform(to: HTTPStatus.noContent)
 	}
 }
