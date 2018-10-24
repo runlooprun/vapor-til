456f6729efaf98b2041c7ce05a0b63ee3188cecd
kazki <ili.o0o@me.com>
2018-10-24 22:00:23 +0900

    Use AcronymsController to handle all "acronyms" routes

--- Sources/App/Controllers/AcronymsController.swift
+++ Sources/App/Controllers/AcronymsController.swift
@@ -4,10 +4,81 @@ import Fluent
 struct AcronymsController: RouteCollection {
 	
 	func boot(router: Router) throws {
-		router.get("api", "acronyms", use: self.getAllHandler)
+		let acronymsRoutes = router.grouped("api", "acronyms")
+		acronymsRoutes.get(use: self.getAllHandler)
+		acronymsRoutes.post(use: self.createHandler)
+		acronymsRoutes.get(Acronym.parameter, use: self.getHandler)
+		acronymsRoutes.put(Acronym.parameter, use: self.updateHandler)
+		acronymsRoutes.delete(Acronym.parameter, use: self.deleteHandler)
+		acronymsRoutes.get("search", use: self.searchHandler)
+		acronymsRoutes.get("first", use: self.getFirstHandler)
+		acronymsRoutes.get("sorted", use: self.sortedHandler)
 	}
 	
+	/// Get all acronyms
 	func getAllHandler(_ req: Request) throws -> Future<[Acronym]> {
 		return Acronym.query(on: req).all()
+	}
+	
+	/// Create (Post) a new acronym
+	func createHandler(_ req: Request) throws -> Future<Acronym> {
+		return try req.content.decode(Acronym.self)
+			.flatMap(to: Acronym.self) { acronym in
+				return acronym.save(on: req)
+			}
+	}
+	
+	/// Get an acronym specified by id
+	func getHandler(_ req: Request) throws -> Future<Acronym> {
+		return try req.parameters.next(Acronym.self)
+	}
+	
+	/// Update (Put) an acronym specified by id
+	func updateHandler(_ req: Request) throws -> Future<Acronym> {
+		return try flatMap(to: Acronym.self, req.parameters.next(Acronym.self), req.content.decode(Acronym.self)) { acronym, updatedAcronym in
+			acronym.short = updatedAcronym.short
+			acronym.long = updatedAcronym.long
+			return acronym.save(on: req)
+		}
+	}
+	
+	/// Delete an acronym specified by id
+	func deleteHandler(_ req: Request) throws -> Future<HTTPStatus> {
+		return try req.parameters.next(Acronym.self)
+			.delete(on: req) // “Fluent allows you to call delete(on:) directly on that Future. This helps tidy up code and reduce nesting. Fluent provides convenience functions for delete, update, create and save.”
+			.transform(to: HTTPStatus.noContent)
+	}
+	
+	/// Search acronym by term (short or long)
+	func searchHandler(_ req: Request) throws -> Future<[Acronym]> {
+		guard
+			let searchTerm = req.query[String.self, at: "term"]
+		else {
+			throw Abort(.badRequest)
+		}
+		
+		return Acronym.query(on: req).group(.or) { or in
+			or.filter(\.short == searchTerm)
+			or.filter(\.long == searchTerm)
+		}.all()
+	}
+	
+	/// Get first acronym
+	func getFirstHandler(_ req: Request) throws -> Future<Acronym> {
+		return Acronym.query(on: req)
+			.first()
+			.map(to: Acronym.self) { acronym in
+				guard let acronym = acronym else {
+					throw Abort(.notFound)
+				}
+				return acronym
+			}
+	}
+	
+	/// Get sorted acronyms
+	func sortedHandler(_ req: Request) throws -> Future<[Acronym]> {
+		return Acronym.query(on: req)
+			.sort(\.short, .ascending)
+			.all()
 	}
 }
