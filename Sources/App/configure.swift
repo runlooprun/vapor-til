fa99c3c42de5bbd3ebb8bcceaf17ba7b2d5525d4
kazki <ili.o0o@me.com>
2018-10-26 16:49:40 +0900

    Register the fluent command config as a service

--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -3,21 +3,21 @@ import Vapor
 
 /// Called before your application initializes.
 public func configure(_ config: inout Config, _ env: inout Environment, _ services: inout Services) throws {
-	/// Register providers first
+	// Register providers first
 	try services.register(FluentPostgreSQLProvider())
 
-	/// Register routes to the router
+	// Register routes to the router
 	let router = EngineRouter.default()
 	try routes(router)
 	services.register(router, as: Router.self)
 
-	/// Register middleware
+	// Register middleware
 	var middlewares = MiddlewareConfig() // Create _empty_ middleware config
-	/// middlewares.use(FileMiddleware.self) // Serves files from `Public/` directory
+	// middlewares.use(FileMiddleware.self) // Serves files from `Public/` directory
 	middlewares.use(ErrorMiddleware.self) // Catches errors and converts to HTTP response
 	services.register(middlewares)
 
-	/// Register the configured SQLite database to the database config.
+	// Register the configured SQLite database to the database config.
 	var databases = DatabasesConfig()
 	let databaseConfig = PostgreSQLDatabaseConfig(
 		hostname: Environment.get("DATABASE_HOSTNAME") ?? "localhost",
@@ -29,9 +29,14 @@ public func configure(_ config: inout Config, _ env: inout Environment, _ servic
 	databases.add(database: database, as: .psql)
 	services.register(databases)
 
-	/// Configure migrations
+	// Configure migrations
 	var migrations = MigrationConfig()
 	migrations.add(model: User.self, database: .psql)
 	migrations.add(model: Acronym.self, database: .psql)
 	services.register(migrations)
+	
+	// Register the fluent command config as a service
+	var commandConfig = CommandConfig.default()
+	commandConfig.useFluentCommands()
+	services.register(commandConfig)
 }
